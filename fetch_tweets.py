400e5eb88286bc38833eb3d6415c7518c2c38134
Matias Insaurralde <matt@scrapinghub.com>
2014-05-16 04:08:29 -0400

    x

--- fetch_tweets.py
+++ fetch_tweets.py
@@ -1,12 +1,12 @@
 from Queue import Queue
 from threading import Thread
 from lxml import html
-import datetime
-import urllib2
+import urllib2, socket
 
 num_fetch_threads = 4
 topsy_per_page = 100
 queue = Queue(4)
+r = redis.StrictRedis(host='107.170.253.33')
 
 def fetch_tweets(query, cursor=None):
   base_url = "https://twitter.com/search?f=realtime&q={0}&src=typd".format(query)
@@ -43,7 +43,8 @@ for i in range(num_fetch_threads):
   worker.setDaemon(True)
   worker.start()
 
-f = open('kickstarter_urls'); lines = f.readlines(); f.close()
+hostname = socket.gethostname()
+f = open(hostname); lines = f.readlines(); f.close()
 
 for line in lines:
   queue.put(line.rstrip())
